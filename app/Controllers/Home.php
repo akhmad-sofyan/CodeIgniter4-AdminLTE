671790edd7091f5c33be16c75202e6dd1b00f9cf
akhmad-sofyan <akhmadsofyanz@gmail.com>
2021-07-20 12:33:15 +0700

    testing template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data['page'] = 'page/dashboard';
+		return view('web', $data);
     }
 }
